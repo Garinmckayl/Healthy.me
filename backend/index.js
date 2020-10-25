2d58a72408c09cc8ac7d1e93de75b1447b4ec61e
Sai Hegde <saihegde21@gmail.com>
2020-10-25 12:13:41 -0600

    Root Endpoint

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,11 @@ app.listen(HTTP_PORT, () => {
   console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.status(200).send({"message":"I am the Healthy.me API"})
+});
+
 app.use(function(req, res) {
     res.status(404).send('Page not Found');
 });
