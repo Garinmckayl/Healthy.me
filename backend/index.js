9c22061ea0ab6960a81ff5adfed40cb86edf89c8
Sai Hegde <saihegde21@gmail.com>
2020-10-25 12:08:14 -0600

    Error Handlers

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,10 @@ app.listen(HTTP_PORT, () => {
   console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
+app.use(function(req, res) {
+    res.status(404).send('Page not Found');
+});
+
+app.use(function(error, req, res, next) {
+    res.status(500).send('Internal Server Error');
+});
